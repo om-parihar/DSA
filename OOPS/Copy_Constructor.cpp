ef5c9974a5f5f56ecb4ee4c89ae90b9fa41bbab2
Om Parihar <Omshivoham191@gmail.com>
2026-01-30 12:27:42 +0530

    OOPs Files Added

---
+++ OOPS/Copy_Constructor.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Customer{
+    string name;
+    int acc_number, balance;
+
+    public:
+    Customer(string a, int b, int c){
+        name=a;
+        acc_number=b;
+        balance=c;
+    }
+
+    Customer(){
+
+    }
+
+    //Copy Constructor
+
+    Customer(Customer &B){
+        name=B.name;
+        acc_number=B.acc_number;
+        balance=B.balance;
+    }
+
+    void display(){
+        cout<<name<<" "<<acc_number<<" "<<balance<<endl;
+    }
+};
+
+int main(){
+    Customer A1("Rahul",123,1000);
+    A1.display();
+
+    Customer A2(A1);
+    A2.display();
+
+    Customer A3;
+    A3=A1;
+    A3.display();
+}
--- OOPS/tempCodeRunnerFile.cpp
+++ OOPS/tempCodeRunnerFile.cpp
@@ -1,5 +1,2 @@
-Customer(string name, int acc_number, int balance){
-    //     this->name=name;
-    //     this->acc_number=acc_number;            //this-> (address store karta hai) is used to tell name is of main class and other name is parameter
-    //     this->balance=balance;
-    // }
+
+    public:
