81e794ccaec7ab9c4afd963d93775e401fe410d9
Om Parihar <Omshivoham191@gmail.com>
2026-02-03 10:53:08 +0530

    OOP Files Added

---
+++ OOPS/Multipath_inheritance.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Human{
+    public:
+    string name;
+    void display(){
+        cout<<"I am "<<name<<endl;
+    }
+};
+
+class Student: public virtual Human{        //virual is used to resolve the same name conflict problem
+    public:
+    string subject;
+    void stu_sub(){
+        cout<<"Currently i am studying "<<subject<<endl;
+    } 
+};
+
+class Male:public virtual Human, public Student{
+    public:
+    //default constructor
+    Male(){
+        
+    };
+
+    Male(string name, string subject){
+        this->name=name;
+        this->subject=subject;
+    }
+    void dis(){
+        cout<<"my name is "<<name<<" currently studying "<<subject<<endl;
+    }
+};
+
+int main(){
+    Male A("King","DSA");
+    A.dis();
+
+}
